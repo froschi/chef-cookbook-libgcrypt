f4204258b9db601a85250896ef3a2536334a5de7
Thorsten Fischer <thorsten@froschi.org>
2012-06-11 13:45:42 +0200

    Amended for use on lucid, precise.
    Signed-off-by: Thorsten Fischer <thorsten@froschi.org>

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,6 +1,15 @@
-packages = %w/
-  libgcrypt11
-/
+packages = Array.new
+
+case node[:lsb][:codename]
+when "lucid"
+  packages |= %w/
+    libgcrypt11
+  /
+when "precise"
+  packages |= %w/
+    libgcrypt11
+  /
+end
 
 packages.each do |pkg|
   package pkg do
